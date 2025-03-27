82ff4e68bb88259f525100a5d7c085ee2eb451d4
TeodoraRistovic13 <128620013+TeodoraRistovic13@users.noreply.github.com>
2025-03-27 19:07:38 +0100

    Update orders.sol

--- solidity/orders.sol
+++ solidity/orders.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
 contract OrderContract {
@@ -7,15 +6,14 @@ contract OrderContract {
     bool public paid;
     bool public delivered;
     address payable public courier;
-    address payable public owner; // Added store owner address
+    address payable public owner;
 
     enum State { CREATED, PAID, DELIVERED, CLOSED }
     State public state;
 
     mapping(State => string) stateNames;
 
-
-     modifier onlyInState(State _state) {
+    modifier onlyInState(State _state) {
         require(state == _state, stateNames[state]);
         _;
      }
@@ -28,8 +26,8 @@ contract OrderContract {
     constructor(address payable _buyer, uint256 _orderAmount) {
         buyer = _buyer;
         orderAmount = _orderAmount;
-        courier = payable(address(0));  // Initialize courier as empty
-        owner = payable(msg.sender); // Assign the contract deployer as the store owner
+        courier = payable(address(0));
+        owner = payable(msg.sender);
         state = State.CREATED;
         stateNames[State.CREATED] = "CREATED";
         stateNames[State.PAID] = "PAID";
@@ -37,14 +35,11 @@ contract OrderContract {
         stateNames[State.CLOSED] = "CLOSED";
     }
 
-    // Function for the buyer to make payment for the order
     function makePayment() public payable onlyBuyer onlyInState(State.CREATED) {
-
         require(msg.value == orderAmount, "Payment amount does not match the order amount");
         state = State.PAID;
     }
 
-    // Function for couriers to accept the order for delivery
     function acceptOrder(address payable _courier) public onlyInState(State.PAID) {
 
         require(courier == address(0), "Order already assigned to a courier");
@@ -52,13 +47,10 @@ contract OrderContract {
         state = State.DELIVERED;
     }
 
-    // Function for the buyer to confirm delivery
     function confirmDelivery() public onlyBuyer onlyInState (State.DELIVERED) {
-
         uint256 ownerShare = (orderAmount * 80) / 100;
         owner.transfer(ownerShare);
         courier.transfer(orderAmount - ownerShare);
-
         state = State.CLOSED;
     }
 }
